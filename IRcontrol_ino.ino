faa4a0a77cd10bfe700dad63646fc1277cf1632a
Th0r4x <thorax909@gmail.com>
2013-12-21 22:43:56 +0100

    Updated version IR remote

--- IRcontrol_ino.ino
+++ IRcontrol_ino.ino
@@ -1,14 +1,63 @@
 #include <IRremote.h>
+#include <vector>
 
 const int RECV_PIN = 50;
+const int BROJ_DIODA = 3;
 int led1 = 10;
 int led2 = 11;
 int led3 = 12;
 
 
+
 IRrecv irrecv(RECV_PIN);
 
 decode_results results;
+
+class Dioda {
+  public:
+    boolean state;
+    int rBr;
+    int code1;
+    int code2;
+    
+    Dioda() {
+    }
+    
+    Dioda(int broj) {
+     rBr = broj;
+     state = false;
+    }
+    
+    void setDiode(int broj, boolean stanje, int kod1, int kod2) {
+      rBr = broj;
+      state = stanje;
+      code1 = kod1;
+      code2 = kod2;   
+      if(stanje == true) {
+        digitalWrite(rBr, HIGH);
+        state = true;
+      }   
+    }
+    
+    void toggleState() {
+      if(state == false) {
+        digitalWrite(rBr, HIGH);
+        state = true;
+      }
+      else {
+        digitalWrite(rBr, LOW);
+        state = false;
+      }
+      
+      Serial.println(state);
+    }
+};
+
+Dioda dioda1 = Dioda(led1);
+Dioda polje[3];
+
+
+
 
 void setup()
 {
@@ -19,8 +68,9 @@ void setup()
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
-
-  
+  polje[0].setDiode(led1, false, 0x001, 0x801);
+  polje[1].setDiode(led2, true, 0x002, 0x802);
+  polje[2].setDiode(led3, false, 0x003, 0x803);
 }
 
 void pinOn(int z) // turns on digital pin "pin" for 1 second
@@ -41,27 +91,26 @@ void loop() {
      if (results.decode_type == RC5) {
       Serial.print("RC5: ");
       Serial.println(results.value, HEX);
-  switch(results.value)
-{
- case 0x001: pinOn(10); break; // 10
-case 0x002: pinOn(11); break; // 11
-case 0x003: pinOn(12); break; // 12
-case 0x801: pinOff(10); break; // 10
-case 0x802: pinOff(11); break; // 11
-case 0x803: pinOff(12); break; // 12
+//  switch(results.value)
+//{
+// case 0x001: dioda1.toggleState(); break; // 10
+//case 0x002: pinOn(11); break; // 11
+//case 0x003: pinOn(12); break; // 12
+//case 0x801: dioda1.toggleState(); break; // 10
+//case 0x802: pinOff(11); break; // 11
+//case 0x803: pinOff(12); break; // 12
+//}
+for(int i=0; i < BROJ_DIODA; i++) {
+ if(polje[i].code1 == results.value || polje[i].code2 == results.value) {
+   polje[i].toggleState();
+ } 
 }
      }
-
- 
- 
-    
-      
-
-    
    
     irrecv.resume(); // Receive the next value
   }
   }
+
 
 }
 
